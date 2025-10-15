ee28d9b303cd3307c5b8b5796dbd1c28ed5bbc59
PALOALTONETWORK\akumar27 <akumar27@paloaltonetworks.com>
2025-10-14 20:01:05 -0700

    Initial commit - Task Manager Pro v1.0

---
+++ sw.js
@@ -0,0 +1,57 @@
+// Service Worker for Task Manager Pro PWA
+const CACHE_NAME = 'task-manager-pro-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './app.js',
+    './styles.css',
+    './manifest.json',
+    'https://unpkg.com/react@18/umd/react.development.js',
+    'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
+    'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+// Install event - cache resources
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache.map(url => new Request(url, {cache: 'reload'})));
+            })
+            .catch(function(error) {
+                console.log('Cache install failed:', error);
+            })
+    );
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Return cached version or fetch from network
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
